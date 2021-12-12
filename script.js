94888b9e2692b5bbd2a36fdd6ee161b034bfadce
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-12-12 18:37:10 +0100

    Add mobile-nav

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ const scrollFunction = () => {
 };
 
 const topFunction = () => (document.documentElement.scrollTop = 0);
+
+const hamburger = document.getElementById('hamburger');
+const mobileNav = document.querySelector('nav.mobile-nav');
+document
+  .getElementById('hamburger')
+  .addEventListener('click', navStatus);
+
+const mobileNavLinks = document.querySelectorAll(
+  'nav.mobile-nav > ul > li > a',
+);
+mobileNavLinks.forEach((link) =>
+  link.addEventListener('click', navStatus),
+);
+
+function navStatus() {
+  if (document.body.classList.contains('hamburger-active')) {
+    navClose();
+  } else {
+    navOpen();
+  }
+}
+
+function navClose() {
+  document.body.classList.remove('hamburger-active');
+  hamburger.classList.remove('fixed');
+  hamburger.classList.add('relative');
+  mobileNav.classList.add('hidden');
+}
+
+function navOpen() {
+  document.body.classList.add('hamburger-active');
+  hamburger.classList.add('fixed');
+  hamburger.classList.remove('relative');
+  mobileNav.classList.remove('hidden');
+}
